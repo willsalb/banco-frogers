2b3ae14124715f48e5d4777ec581fe2237996f04
will <willamssouza917@gmail.com>
2022-08-03 16:09:06 -0300

    new file:   .editorconfig

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+interface IContaBancaria {
+  nomeTitular: string
+  numero: number
+  agencia: number
+  saldo?: number
+  tipo: string
+}
+
+class ContaBancaria {
+   nomeTitular: string
+   tipo: string
+   numero: number
+   agencia: number
+   saldo: number
+  private ativa: boolean = true
+
+  constructor ({nomeTitular, saldo= 0, numero, agencia, tipo}: IContaBancaria) {
+    this.nomeTitular = nomeTitular
+    this.tipo =  tipo
+    this.numero = numero
+    this.saldo = saldo
+    this.agencia = agencia
+    console.log(`${this.nomeTitular}, Bem vindo ao banco Frogers`)
+  }
+
+  meuSaldo = () => this.saldo
+
+  depositar = (valor: number): void | boolean => {
+    if(!this.ativa){
+      console.log('Conta desativada, impossível realizar depósito')
+      return this.ativa
+    };
+    this.saldo += valor;
+    console.log(`Você depositou ${valor}, seu saldo agora é: ${this.saldo}`)
+  }
+
+  sacar = (valor: number): boolean => {
+    if(!this.ativa){
+      console.log('Conta desativada, impossível realizar saque')
+      return this.ativa
+    }
+
+    if(valor <= this.saldo) {
+      this.saldo = this.saldo - valor;
+      console.log(`Você sacou ${valor}, seu saldo agora é: ${this.saldo}`)
+      return this.ativa
+    }
+
+    console.log(`Operação negada! Saldo insuficiente. Você possui ${this.saldo} na sua conta`)
+    return this.ativa
+  }
+
+  CancelarConta = () => {
+    if(this.saldo === 0) {
+      this.ativa = false
+      console.log('Conta desativada')
+      return this.ativa
+    }
+    console.log(`Não é possivel desavitar sua conta. Conta com pendências`)
+  }
+}
+
+/*const Conta1:ContaBancaria = new ContaBancaria({nomeTitular: 'will', agencia: 444, numero: 321, tipo: 'conrrente'})
+Conta1.depositar(500)
+
+Conta1.sacar(200)
+console.log(Conta1.meuSaldo())*/
+
+//Conta1.CancelarConta()
+//Conta1.depositar(100)
